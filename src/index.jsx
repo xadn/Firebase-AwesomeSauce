36cab93f34a293c4661daf3d8281855134b8c6d5
Andy Niccolai <andy.niccolai@gmail.com>
2014-04-04 00:13:04 -0600

    autosave changes to messages

--- src/index.jsx
+++ src/index.jsx
@@ -3,31 +3,81 @@ var React = require('react/addons'),
 
 var myDataRef = new Firebase('https://dymcvl2kwer.firebaseio-demo.com/');
 
-// var Message = React.createClass({
-//   mixins: [React.addons.LinkedStateMixin],
+var AutosaveInput = React.createClass({
+  getInitialState: function() {
+    return { value: '' };
+  },
 
-//   getInitialState: function() {
-//     return { name: '', text: '' };
-//   },
+  componentWillMount: function() {
+    this.props.valueRef.on('value', this.receiveChange);
+  },
 
-//   handleSubmit: function() {
-//     return false;
-//   },
+  componentWillUnmount: function() {
+    this.props.valueRef.off('value', this.receiveChange);
+  },
 
-//   handleTextChange: function() {
+  receiveChange: function(snapshot) {
+    this.setState({ value: snapshot.val() });
+  },
 
-//   },
+  sendChange: function(e) {
+    this.props.valueRef.set(e.target.value);
+  },
 
-//   render: function() {
-//     return (
-//       <form onSubmit={this.handleSubmit}>
-//         <span>Name: {this.state.name}</span>
-//         <input type='text' value={this.state.text} onChange={this.handleTextChange} placeholder='Message' />
-//         <input type='submit' value='Post' />
-//       </form>
-//     );
-//   }
-// });
+  render: function() {
+    return <input type='text' value={this.state.value} onChange={this.sendChange} placeholder={this.props.placeholder} />
+  }
+});
+
+var AutoLabel = React.createClass({
+  getInitialState: function() {
+    return { value: '' };
+  },
+
+  componentWillMount: function() {
+    this.props.valueRef.on('value', this.receiveChange);
+  },
+
+  componentWillUnmount: function() {
+    this.props.valueRef.off('value', this.receiveChange);
+  },
+
+  receiveChange: function(snapshot) {
+    this.setState({ value: snapshot.val() });
+  },
+
+  render: function() {
+    return <span>{this.state.value}</span>
+  }
+});
+
+var Message = React.createClass({
+  mixins: [React.addons.LinkedStateMixin],
+
+  getInitialState: function() {
+    return { name: '', text: '' };
+  },
+
+  componentWillMount: function() {
+    this.props.messageRef.on('value', snapshot => {
+      this.setState({ name: snapshot.val().name });
+    });
+  },
+
+  handleTextChange: function(e) {
+    this.props.messageRef.child('text').set(e.target.value);
+  },
+
+  render: function() {
+    return (
+      <div>
+        <span>Name: {this.state.name}</span>
+        <AutosaveInput type='text' placeholder='Name' valueRef={this.props.messageRef.child('text')} />
+      </div>
+    );
+  }
+});
+
 
 var Messages = React.createClass({
   mixins: [React.addons.LinkedStateMixin],
@@ -37,8 +87,9 @@ var Messages = React.createClass({
   },
 
   componentWillMount: function() {
-    this.props.dataRef.on('value', snapshot => {
-      this.setState({messages: snapshot.val()});
+    this.props.dataRef.on('child_added', snapshot => {
+      this.state.messages.push(snapshot);
+      this.setState({ messages: this.state.messages });
     });
   },
 
@@ -51,8 +102,8 @@ var Messages = React.createClass({
   render: function() {
     return (
       <div>
-        {_.map(this.state.messages, function(message, key) {
-          return <div key={key}>Name: {message.name}, Text: {message.text}</div>
+        {_.map(this.state.messages, function(message) {
+          return <Message key={message.name()} messageRef={message.ref()} />
         })}
         <form onSubmit={this.handleSubmit}>
           <input type='text' valueLink={this.linkState('name')} placeholder='Name' />
