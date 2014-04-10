2a9654a5c33651c55ac828d47e4d4fb5374a99fa
Andy Niccolai <aniccolai@pivotallabs.com>
2014-04-10 10:10:11 -1000

    test 3-way merging

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 var React = require('react'),
     Messages = require('./messages');
-
 var rootDataRef = new Firebase('https://dymcvl2kwer.firebaseio-demo.com/');
-
 React.renderComponent(Messages({dataRef: rootDataRef}), document.body);
 
+// var goog = require('diff-match-patch'),
+//     dmp = new goog.diff_match_patch();
+
+// var text1 = 'Hello, World!',
+//     text2 = 'Aloha, World!',
+//     text3 = 'Hello, World how are you!';
+
+// var patch = dmp.patch_make(text1, text2);
+// var applied = dmp.patch_apply(patch, text3);
+
+// console.log(applied);
